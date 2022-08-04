7d6cdfa2810ea9ba60b6155acfd6aafeb290c46b
thiagogagliari <thiagogagliari@hotmail.com>
2022-08-04 18:20:35 -0300

    Exercícios Condicionais 1 a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,20 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = (prompt('Qual sua idade?'));
+if (idade >= 18) {
+    alert('Você é maior de idade!')
+}   else{
+	alert('Vocé é menor de idade!')
+}   
 
-if(){
+let ensinoMedio = confirm('Você terminou o ensino médio? Se sim aperte OK, Caso contrário em CANCELAR!');
+if(ensinoMedio === true) {
+    alert('Terminei o ensino médio!')
+}   else{
+    alert('Não terminei o ensino médio!')
+}
 
-}else{
-	
+let isCursandoOutraFaculdade = confirm('Você está cursando alguma faculdade? Se sim aperte OK, caso conntrário em CANCELAR!');
+if(isCursandoOutraFaculdade === true) {
+    alert('Você está cursando uma faculdade!')
+}   else {
+    alert('Você não está cursando outra faculdade!')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Qual a sua nacionalidade?').toLowerCase()
+
+if(nacionalidade === 'brasileira' || (nacionalidade === 'brasileiro')) {
+    console.log('Sua nacionalidade é: ' + nacionalidade + '!')
+}   else if(nacionalidade === 'argentina' || (nacionalidade === 'argentino')) {
+    console.log('Sua nacionalidade é: ' + nacionalidade + '!')
+}   else if(nacionalidade === 'uruguaia' || (nacionalidade === 'uruguaio')) {
+    console.log('Sua nacionalidade é: ' + nacionalidade + '!')
+}   else if(nacionalidade === 'chilena' || (nacionalidade === 'chileno')) {
+    console.log('Sua nacionalidade é: ' + nacionalidade + '!')
+}   else if(nacionalidade === 'colombiana' || (nacionalidade === 'colombiano')) {
+    console.log('Sua nacionalidade é: ' + nacionalidade + '!')
+}   else {
+    console.log('Nacionalidade não encontrada')
+}
